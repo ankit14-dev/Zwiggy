08183f5349e0ad4534bb1273f78e769ea7615276
Ankit <ankitkr.ak14@gmail.com>
2026-02-01 03:07:06 +0530

    removed proxy

--- FoodDeliveryFrontend/vite.config.js
+++ FoodDeliveryFrontend/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://food-delivery-backend-ckyd.onrender.com',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://food-delivery-backend-ckyd.onrender.com',
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
   },
 })
